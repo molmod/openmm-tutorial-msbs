1adc4d0e57a3651c67e671161c4660650550b31f
Toon Verstraelen <toon.verstraelen@UGent.be>
2020-12-19 12:13:55 +0100

    Minor improvements

---
+++ runall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Strip output cells from notebooks
+for file in $(git ls-files | grep 'ipynb$' | grep -v '^03'); do
+  echo Running ${file}
+  jupyter nbconvert ${file} --to notebook --execute --ExecutePreprocessor.timeout=-1
+done
