d8e3847f2b7c24f1769b52138c4b1dec24ca874b
jeveki <70133675+jeveki@users.noreply.github.com>
2023-11-15 12:37:29 +0100

    Update to newer python version

--- 03_noninteractive/job_openmm_vsc_gpu.sh
+++ 03_noninteractive/job_openmm_vsc_gpu.sh
@@ -8,7 +8,7 @@
 
 # Setup our OpenMM environment
 module load OpenMM/8.0.0-foss-2022a-CUDA-11.7.0 MDTraj/1.9.7-foss-2022a matplotlib/3.5.2-foss-2022a jax/0.3.25-foss-2022a lxml/4.9.1-GCCcore-11.3.0 PyYAML/6.0-GCCcore-11.3.0
-. ${VSC_DATA}/venvs/${VSC_ARCH_LOCAL}/3.9.5-GCCcore-10.3.0/bin/activate
+. ${VSC_DATA}/venvs/${VSC_ARCH_LOCAL}/3.10.4-GCCcore-11.3.0/bin/activate
 
 # Set the number of threads.
 export OPENMM_CPU_THREADS=${SLURM_CPUS_PER_TASK}
